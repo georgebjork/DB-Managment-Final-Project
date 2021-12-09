3bc21ad33c92a7573d3922d03776413b6aaf80e9
George Bjork <georgebjork@outlook.com>
2021-12-09 10:37:59 -0800

    added more changes

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace DB_Managment_Final_Project
     {
         static void Main(string[] args)
         {
+
             String select = "SELECT s.name ";
             String from = "FROM School s ";
             String where = "WHERE ";
@@ -17,43 +18,84 @@ namespace DB_Managment_Final_Project
             String command = "";
             Console.Write("Do you want to connect to the database(y/n): ");
             command = Console.ReadLine();
-            if(command == "y"){
+            if (command == "y")
+            {
                 db.connect();
             }
-            
+
             Console.WriteLine("Welcome to the college finder! This application will help you find a college that is right for you!");
             Console.WriteLine();
 
-            //display a list of filter 
-                //state
-                //tuitition 
-                //etc
+            while (command != "stop")
+            {
 
-            Console.WriteLine("[0] Filter by State");
-            Console.WriteLine("[1] Filter by major name");
-            Console.WriteLine("[2] Filter by Degree name");
-            Console.WriteLine("[3] Filter by EC");
-            Console.WriteLine();
+                Console.WriteLine("[0] Filter by State");
+                Console.WriteLine("[1] Filter by major name");
+                Console.WriteLine("[2] Filter by Degree name");
+                Console.WriteLine("[3] Filter by EC");
+                Console.WriteLine();
 
-            Console.Write("Select an option: ");
-            command = Console.ReadLine();
 
-            if(command == "0"){
-                Console.Write("Enter a state abbeviation: ");
+                Console.Write("Select an option: ");
                 command = Console.ReadLine();
+                Console.WriteLine();
 
-                from = from + ", Address a ";
-                where = where + "a.state = \'" + command + "\'" + " AND s.id = a.school_id ";
+                if (command == "0")
+                {
+                    while (command != "quit")
+                    {
+                        Console.WriteLine("Type quit to quit");
+                        Console.Write("Enter a state abbeviation: ");
+                        command = Console.ReadLine();
+                        Console.WriteLine();
+
+                        db.Execute("exec sortState " + "\'" + command + "\'");
+                    }
+
+                }
+                else if (command == "1")
+                {
+                    while (command != "quit")
+                    {
+                        Console.WriteLine("Type quit to quit");
+                        Console.Write("Enter the name of a major (ex: Computer Science): ");
+                        command = Console.ReadLine();
+
+                        db.Execute("exec schoolWithMajor " + "\'" + command + "\'");
+                    }
+
+                }
+                else if (command == "2")
+                {
+                    while (command != "quit")
+                    {
+                        Console.WriteLine("Type quit to quit");
+                        Console.Write("Enter the name of a degree (ex: Masters): ");
+                        command = Console.ReadLine();
+
+                        db.Execute("exec AllData_OnDegree " + "\'" + command + "\'" +  " 'Degree' " );
+                    }
+                }
+
+                else if (command == "3")
+                {
+
+                }
+
+
+                else
+                {
+                    Console.Write( command + " is not a valid command.");
+                }
+
             }
 
-            String q = select + from + where;
-
-            db.Execute(q);
-
-        } 
 
 
-            
-            
+        }
+
+
+
+
     }
 }
